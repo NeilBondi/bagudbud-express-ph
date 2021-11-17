d14b5ea1fb17b7908a19f1330ddaafe1adef7ef2
Wayne <wayne@example.com>
2021-11-17 20:03:52 +0800

    fixed swapped details

--- app/Views/client/displayNotificationlist.php
+++ app/Views/client/displayNotificationlist.php
@@ -4,8 +4,9 @@
 
         $date = date_create($row['ndate']);
         $xdate = date_format($date, "F j, Y, g:i a");   
-    
+        if ($row['status'] == '1') {
 ?>
+
 <tr class="items" data-label="item-1" style="cursor: pointer;">
     <td>
         <div class="d-flex align-items-center py-1 py-xxl-3">
@@ -20,5 +21,21 @@
     </td>
 </tr> 
 <?php
+        } else {
+?>
+<tr class="items" data-label="item-1" style="cursor: pointer;">
+    <td>
+        <div class="d-flex align-items-center py-1 py-xxl-3 ms-4">
+            <div class="request-item-content w-100 ms-4">
+                <div class="left-content">
+                    <p class="text-black fw-bold m-0"><?php echo $row['sender'];?></p>
+                    <span class="display-7"><?php echo $xdate?></span>
+                </div>
+            </div>
+        </div>
+    </td>
+</tr> 
+<?php
+        }
     }
 ?>
